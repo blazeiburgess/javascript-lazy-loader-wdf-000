ceb0c4f64cab941caaa41abbacf6913137568f94
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-18 17:35:32 -0400

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,6 +1,8 @@
 "use strict";
 
 $(document).ready(function() {
-  // add click listener here
-  // it should call on fetchJSON()
+  $('button#load-cars').on('click', function(event) {
+    var page = 2;
+    fetchJSON();
+  });
 });
